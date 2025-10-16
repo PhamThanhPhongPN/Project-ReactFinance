ce286997572e7300e1208caf1c106af985ea137f
Phong <thanhphong102006@gmail.com>
2025-10-16 10:01:11 +0700

    temp1

--- src/RouterConfig.tsx
+++ src/RouterConfig.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import SignUp from './components/forms/SignUp'
+import SignIn from './components/forms/SignIn'
 
 export default function RouterConfig() {
   return (
     <div>
       <Routes>
         <Route path='sign-up' element={<SignUp/>}></Route>
+        <Route path='sign-in' element={<SignIn/>}></Route>
       </Routes>
     </div>
   )
--- src/components/forms/SignUp.tsx
+++ src/components/forms/SignUp.tsx
@@ -1,18 +1,74 @@
-import React from "react";
+import { useState, type FormEvent } from "react";
 import "./SignUp.css";
 
 export default function SignUp() {
+  const [fullName, setFullName] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [errors, setErrors] = useState({fullName: "", password: "", confirmPassword: ""})
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    let valid = true
+
+    const newErrors = {
+      fullName: "",
+      password: "",
+      confirmPassword: ""
+    }
+
+    if (fullName.length == 0) {
+      valid = false
+      newErrors.fullName = "Please enter your username ..."
+    }
+
+    if (password.length == 0) {
+      valid = false
+      newErrors.password = "Please enter your password ..."
+    }
+
+    if (confirmPassword.length == 0) {
+      valid = false
+      newErrors.confirmPassword = "Please enter your confirm password ..."
+    }
+
+    if (password.length < 6) {
+      valid = false
+      newErrors.password = "Password must be more than 6 characters!"
+    }
+
+    if (confirmPassword != password) {
+      valid = false
+      newErrors.confirmPassword = "Confirm password must be the same as password!"
+    }
+
+    setErrors(newErrors)
+    if (valid) {
+      console.log("Đăng nhập thành công!")
+    }
+  }
+
   return (
     <div className="signup-page">
       <div className="signup-box">
         <div className="text-box">
           Sign Up
         </div>
-        <form className="signup-form">
-          <input type="text" className="form-input" placeholder="Username here ..."/>
-          <input type="password" className="form-input" placeholder="Password here ..."/>
-          <input type="password" className="form-input" placeholder="Confirm password here ..."/>
-          <button className="submit-btn">Sign Up</button>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <input type="text" name="fullName" onChange={(e) => {
+            setFullName((e.target as any).fullName.value)
+          }} className={`form-input ${errors.fullName ? "error" : ""}`} placeholder="Username here ..." />
+          {errors.fullName && <p className="error-text">{errors.fullName}</p>}
+          <input type="password" name="password" onChange={(e) => {
+            setPassword((e.target as any).password.value)
+          }} className={`form-input ${errors.password ? "error" : ""}`} placeholder="Password here ..." />
+          {errors.password && <p className="error-text">{errors.password}</p>}
+          <input type="password" name="confirmPassword" onChange={(e) => {
+            setConfirmPassword((e.target as any).confirmPassword.value)
+          }} className={`form-input ${errors.confirmPassword ? "error" : ""}`} placeholder="Confirm password here ..." />
+          {errors.confirmPassword && <p className="error-text">{errors.confirmPassword}</p>}
+          <button className="submit-btn" type="submit">Sign Up</button>
         </form>
         <div className="signup-footer">
           <div className="footer1">
